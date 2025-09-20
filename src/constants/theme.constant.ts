33991fc896f7991a7f888e32f90d02031cc8cb00
Sarunya <sarunya46mk@gmail.com>
2025-09-20 13:38:55 +0700

    style: set up theme

---
+++ src/constants/theme.constant.ts
@@ -0,0 +1,11 @@
+import { Moon, Sun } from 'lucide-react'
+export const THEME = {
+  LIGHT: {
+    NAME: 'light',
+    ICON : Sun
+  },
+  DARK: {
+    NAME: 'dark',
+    ICON : Moon
+  }
+} as const
