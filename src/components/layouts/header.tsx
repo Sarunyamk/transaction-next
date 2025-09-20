ff9ec52630b4432cbba7a39455f66cd0ca0f876a
Sarunya <sarunya46mk@gmail.com>
2025-09-20 11:09:02 +0700

    feat: add image

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,7 +1,32 @@
+import { APP_NAME } from '@/config/env.config'
+import { fredoka } from '@/styles/font'
+import Image from 'next/image'
 import React from 'react'
+import { ClipboardList, Plus } from 'lucide-react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header className="px-4 py-2 sticky border-b flex justify-between items-center">
+      <div className="flex gap-2 items-center">
+        <Image
+          src="/logo.png"
+          width={40}
+          height={40}
+          alt="Logo"
+          className="w-10 h-10"
+        />
+        <h3 className={`${fredoka.className} text-primary text-2xl font-bold`}>
+         {APP_NAME}
+        </h3>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <a><ClipboardList /></a>
+        <a><Plus /></a>
+        {/* <SearchButton /> */}
+        {/* <ThemeToggle /> */}
+        {/* <UserMenu /> */}
+      </div>
+    </header>
   )
 }
--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,4 +1,7 @@
-import { Roboto } from 'next/font/google'
+import { Fredoka, Roboto } from 'next/font/google'
 export const roboto = Roboto({
+  subsets: ['latin'],
+})
+export const fredoka = Fredoka({
   subsets: ['latin'],
 })
